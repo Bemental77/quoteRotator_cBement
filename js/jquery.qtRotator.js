65ecd3db4c96417b9e99e2a40a895232de0ede55
Bemental77 <bementcw@gmail.com>
2022-02-24 21:28:30 -0600

    Creating a jQuery plugin

---
+++ js/jquery.qtRotator.js
@@ -0,0 +1,42 @@
+;(function($, window, undefined) {
+
+
+
+
+
+
+
+
+    $.fn.qtRotator = function (options) {
+
+
+
+        if (typeof options === 'string') {
+            
+            // not as common, leave off code for now...
+            
+        }
+        else {  // options !== 'string', usually meaning it is an object
+            
+            // here, this refers the jQuery object that was used
+            // to call this plugin method ($('#quoteRotator'))
+            this.each(function() {
+                
+                var instance = $.data(this, 'qtRotator');
+                
+                if (instance) {
+                    instance._init();
+                }
+                else {
+                    
+                    
+                    
+                }
+                
+            });
+            
+        }
+
+    };
+
+})(jQuery, window);
