1e96bf28cd739408bd4cf03d1e6918e40205f9e7
Bemental77 <bementcw@gmail.com>
2022-03-03 22:54:40 -0600

    Standard jQuery plugin

--- js/jquery.qtRotator.js
+++ js/jquery.qtRotator.js
@@ -1,7 +1,77 @@
 ;(function($, window, undefined) {
 
+    'use strict';
+
+    var Modernizr = window.Modernizr;
+
+    //Constructor function definition for our new QTRotator object
+    $.QTRotator = function (options, element) {
+
+        this.$element = $(element);
+        this._init(options);
+    };
+
+    //Set up default options
+    $.QTRotator.defaults = {
+        speed: 700,
+        easing: 'ease',
+        interval: 8000
+    };
+
+    //Create methods for new object
+    $.QTRotator.prototype = {
+        _init: function (options) {
+
+            //this refers to $.QTRotator
+            this.realOptions = $.extend($.QTRotator.defaults, options);
+
+            this._config();
+
+            //show the current quote
+            this.$items.eq(this.currentIndex).addClass('quoteCurrent');
+
+            if (this.support){
+                this._setTransition();
+            }
+
+            //start rotating the quotes
+            this._startRotator();
+
+        },
+        _config: function () {
+
+            this.$items = $('.quoteContent');
+
+            this.itemsCount = this.$items.length;
+
+            this.currentIndex = 0; //index of the current quote
+
+            this.support = Modernizr.csstransitions;
+
+            if (this.support){
+                this.$progressBar = $('<span class="quoteProgress"></span>').appendTo(this.$element);
+            } else {
+                //do alternative code for browsers that don't support css transitions
+            }
+
+        },
+        _setTransition: function () {
+
+            setTimeout($.proxy(function(){
+
+                this.$items.css('transition', 'opacity ' + this.realOptions.speed + 'ms '
+                + this.realOptions.easing);
+                //transition: opacity 500ms ease;
+
+            }, this), 25);
+
+        },
+        _startRotator: function () {
 
 
+
+        }
+    };
 
 
 
@@ -30,8 +100,7 @@
                 }
                 else {
                     
-                    instance = $.data(this, 'qtRotator', new
-                    $.QTRotator(options, this));
+                    instance = $.data(this, 'qtRotator', new $.QTRotator(options, this));
                     
                 }
                 
