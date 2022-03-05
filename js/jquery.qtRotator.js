1d372cc504f2bc2d44bc7fb951afaca63ee93c7d
cbement <bementcw@gmail.com>
2022-03-05 13:48:16 -0600

    Completed jquery plugin, Rotator

--- js/jquery.qtRotator.js
+++ js/jquery.qtRotator.js
@@ -68,7 +68,58 @@
         },
         _startRotator: function () {
 
+            //animate width of progress bar <span> from 0% to 100% over time
+            if(this.support) {
+                this._startProgress();
+            }
 
+            setTimeout($.proxy(function(){
+
+                //reset the progress bar
+                if (this.support){
+                    this._resetProgressBar();
+                }
+
+                //quote animations (fading and bringing in next quote)
+                this._nextQuote();
+
+                //being next rotation using a recursive function call
+                this._startRotator();
+
+            }, this), this.realOptions.interval + 10);
+
+        },
+        _startProgress: function () {
+
+            setTimeout($.proxy(function(){
+
+                this.$progressBar.css({
+                    transition: 'width ' + this.realOptions.interval + 'ms linear',
+                    width: '100%'
+                });
+
+            }, this), 25);
+
+        },
+        _resetProgressBar: function () {
+
+            this.$progressBar.css({
+                transition: 'none',
+                width: '0%'
+            });
+
+        },
+        _nextQuote: function () {
+
+            //hide the current quote by fading it out using our opacity transition
+            this.$items.eq(this.currentIndex).removeClass('quoteCurrent')
+
+            //get the index of next quote to show
+            this.currentIndex = this.currentIndex < this.itemsCount - 1 ?
+                this.currentIndex + 1 : 0;
+
+            //show the next quote using the index
+            this.$items.eq(this.currentIndex).addClass('quoteCurrent');
 
         }
     };
